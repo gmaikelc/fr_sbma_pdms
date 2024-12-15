6c972018fcc7c44b5df61a31f829ee44825ced6f
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-12-14 23:27:59 -0600

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -80,12 +80,12 @@ st.image(image)
 col1, col2, col3 = st.columns(3)
 
 # Input for 'SBMA molecular weight'
-sbma_mw = col1.number_input('SBMA molecular weight', max_value = 6000.0)
+sbma_mw = col1.number_input('SBMA molecular weight')
 if sbma_mw < 280.41 or sbma_mw > 6000.0:
     st.error("SBMA molecular weight must be between 280.41 and 6000.0.")
 
 # Input for 'Percent'
-pdms_mw = col2.number_input('PDMS molecular weight', max_value = 11000.0)
+pdms_mw = col2.number_input('PDMS molecular weight')
 if pdms_mw < 92.12 or pdms_mw > 11000.0:
     st.error("PDMS molecular weight must be between 92.12 and 11000.0.")
 
