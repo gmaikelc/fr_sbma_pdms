8f70f5c01426185b646aa9de0e0f7904ab59f084
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-12-14 20:01:52 -0600

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -98,6 +98,12 @@ fraction_pdms = pdms_mw/pdms_mw_unit
 st.write('fraction sbma:', fraction_sbma)
 st.write('fraction pdms:', fraction_pdms)
 
+data_desc_sbma_pdms = pd.read_csv("data/" + "data_126c_15var_pLC50_train_sw.csv")
+#data_train = pd.read_csv("data/" + "data_126c_15var_pLC50_train_sw.csv")
+#mean_value = data_train['pLC50_sw'].mean()
+#loaded_model = pickle.load(open("models/" + "ml_model_rotifer_sw.pickle", 'rb'))
+#loaded_desc = pickle.load(open("models/" + "ml_descriptor_rotifer_sw.pickle", 'rb'))
+
 
 def generate_si_oil_pattern(choice, sbma_mw, pdms_mw, DM='DM', DP='DP', PM='PM'):
     # Calculate Number_Rep_Unit_1
