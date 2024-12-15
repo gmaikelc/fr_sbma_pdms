9b9136ffaeb35249d9f816e7b6027dda46e66fa3
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-12-14 23:20:34 -0600

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -80,10 +80,10 @@ st.image(image)
 col1, col2, col3 = st.columns(3)
 
 # Input for 'SBMA molecular weight'
-sbma_mw = col1.number_input('SBMA molecular weight', min_value=0, max_value = 6000)
+sbma_mw = col1.number_input('SBMA molecular weight', min_value=280.41, max_value = 6000)
 
 # Input for 'Percent'
-pdms_mw = col2.number_input('PDMS molecular weight', min_value=0, max_value = 11000)
+pdms_mw = col2.number_input('PDMS molecular weight', min_value=92.12, max_value = 11000)
 
 # Dropdown for 'PM' and 'DP'
 options = ['0.2%', '1%','5%']
@@ -122,8 +122,8 @@ def mixture_descriptors(data1, data2):
     fraction_sbma = sbma_mw/sbma_mw_unit
     fraction_pdms = pdms_mw/pdms_mw_unit
     
-    #component1 = data['Component1']
-    #component2 = data['Component2']
+    component1 = fraction_sbma  #data['Component1']
+    component2 = fraction_pdms #data['Component2']
 
     # Multiply corresponding rows in data1 and data2 for all columns
     df_mixture_left = component1.values[:, None] * test_data1.values
@@ -318,132 +318,34 @@ loaded_desc = pickle.load(open("models/" + "descriptor_clyt_10psi_rf.pickle", 'r
 #loaded_desc = pickle.load(open("models/" + "ml_descriptor_rotifer_sw.pickle", 'rb'))
 
 
-def generate_si_oil_pattern(choice, sbma_mw, pdms_mw, DM='DM', DP='DP', PM='PM'):
-    # Calculate Number_Rep_Unit_1
-    st.write('percent:',percent)
-    Number_Rep_Unit_2 = round(percent / 100 * degree_of_polymerization)
+#Footer edit
 
-    
-
-    # Perform the division and get the integer quotient and remainder
-    Number_Rep_Unit_1 = degree_of_polymerization - Number_Rep_Unit_2 - 2  # Assuming a value for DiMethyl for the code to run
-    ratio_rep_unit, remainder = divmod(Number_Rep_Unit_1, Number_Rep_Unit_2 + 1)
-    num_it = Number_Rep_Unit_2
-
-    # Specify the number of times to repeat the pattern
-    num_repeats = num_it
-
-    # Define name_dm, name_pm, and name_dp based on ratio_rep_unit and num_1
-    name_dm = f'-[{DM}({ratio_rep_unit})]'
-    name_pm = f'-[{PM}({num_1})]'
-    name_dp = f'-[{DP}({num_1})]'
-
-    # Construct the pattern based on the choice
-    if choice == 'DP':
-        pattern = f'{name_dm}{name_dp}' * num_repeats
-    elif choice == 'PM':
-        pattern = f'{name_dm}{name_pm}' * num_repeats
-    else:
-        pattern = ''  # Handle invalid choice gracefully
-
-    # Print the pattern along with left and right ends
-    si_oil_pattern = f'{name_left_end}{pattern}{name_dm}{name_right_end}'
-
-    return si_oil_pattern
-
-def generate_si_oil(choice, percent, degree_of_polymerization):
-    # Calculate Number_Rep_Unit_2
-    st.write('percent:',percent)
-    Number_Rep_Unit_2 = round(percent/100 * degree_of_polymerization)
-    st.write(f'Number of repeating units for {choice} :', Number_Rep_Unit_2)
-
-    # Perform the division and get the integer quotient and remainder
-    Number_Rep_Unit_1 = degree_of_polymerization - Number_Rep_Unit_2 - 2  # Assuming a value for DiMethyl for the code to run
-    ratio_rep_unit, remainder = divmod(Number_Rep_Unit_1, Number_Rep_Unit_2 + 1)
-
-    # Print the results
-    st.write("Ratio (Integer part):", ratio_rep_unit)
-    st.write("Remainder:", remainder)
-
-    # Number of iterations
-    num_it = Number_Rep_Unit_2
-    st.write('Number of iterations:', num_it)
-    st.write('RU:', choice)
-
-    # Perform the concatenation for the specified number of iterations
-
-    # Initialize the final string with the left end
-    end_ru = DM * ratio_rep_unit
-    f_ru = ''
-
-    # Depending on the choice, add the string DP or PM at the end of f_ru after each iteration
-    if choice == 'PM':
-        for i in range(num_it):
-            # Add the DM string ratio_DM times
-            f_ru += (DM * ratio_rep_unit) + PM
-
-    if choice == 'DP':
-        for i in range(num_it):
-            # Add the DM string ratio_DM times
-            f_ru += (DM * ratio_rep_unit) + DP
-            
-            
-    n_ru = f_ru + end_ru
-
-    si_oil = left_end + n_ru + right_end
-
-    # Remove the patterns
-    si_oil_final = si_oil.replace('**', '').replace('I*', '')
-
-    return si_oil_final
-
-
-if st.button('Press to see the pattern for the assembled silicon oil'):
-  # Example usage:
-  si_oil_pattern_generated = generate_si_oil_pattern(choice, percent, degree_of_polymerization)
-  st.write(si_oil_pattern_generated)
-
-if st.button('Press to generate the silicon oil structure based on the parameters'):
-
-# Example usage:
-  si_oil_generated = generate_si_oil(choice, percent, degree_of_polymerization)
-  st.write(si_oil_generated)
-
-
-  mol = Chem.MolFromSmiles(si_oil_generated)
-  mol = Chem.AddHs(mol)
-  AllChem.EmbedMolecule(mol, AllChem.ETKDG())
-
- # Save as a mol file
- #save_filename = st.text_input("Enter filename for MOL file")
- #if st.button("Save MOL"):
-   #with open(save_filename, "w") as f:
-     #writer = Chem.SDWriter(f)
-     #writer.write(mol)
-     #writer.close()
-   #st.success(f"Mol file saved as {save_filename}")
- #writer = Chem.SDWriter(filename)
- #writer.write(mol)
- #writer.close()
-
-
-#def smiles_to_mol(smiles):
- #   mol = Chem.MolFromSmiles(smiles)
-  #  if mol:
-   #     mol = Chem.AddHs(mol)
-    #    AllChem.EmbedMolecule(mol, AllChem.ETKDG())
-     #   return mol
-    #else:
-     #   return None
-
-#def save_mol_file(mol, filename):
- #   writer = Chem.SDWriter(filename)
-  #  writer.write(mol)
-   # writer.close()
-
-# Convert SMILES to mol
-#mol = smiles_to_mol(si_oil_final)
-
-# Save as a mol file
-#save_filename = "output.mol"
-#save_mol_file(mol, save_filename)
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Made in  🐍 and <img style='display: ; 
+' href="https://streamlit.io" src="https://i.imgur.com/iIOA6kU.png" target="_blank"></img> Developed by <a style='display: ;
+ text-align: center' href="https://www.linkedin.com/in/gerardo-m-casanola-martin-27238553/" target="_blank">Gerardo M. Casanola</a> for <a style='display: ; 
+ text-align: center;' href="http://www.rasulev.org" target="_blank">Rasulev Research Group</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
