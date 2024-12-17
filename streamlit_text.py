0c090742f41dda3d7bab7a45d8551e7c708ae21d
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-12-16 23:15:55 -0600

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -520,11 +520,11 @@ if run == True:
         col1, col2 = st.columns(2)
 
         with col1:
-            st.header(f"Fouling Release SBMA-PDMS at {percentage}%")
+            #st.header(f"Fouling Release SBMA-PDMS at {percentage}%")
             st.markdown("<hr style='border: 1px solid blue;'>", unsafe_allow_html=True)
             st.subheader(r'Predictions')
             st.write(styled_df)
-            #st.markdown("<h2 style='text-align: center; font-size: 30px;'>William's Plot (Applicability Domain)</h2>", unsafe_allow_html=True)
+            st.markdown("<h2 style='text-align: center; font-size: 30px;'>f'Fouling Release SBMA-PDMS at {percentage}%'</h2>", unsafe_allow_html=True)
             #st.plotly_chart(figure,use_container_width=True)
             #st.markdown(":point_down: **Here you can download the results for Salt Water model**", unsafe_allow_html=True,)
             #st.markdown(filedownload1(final_file), unsafe_allow_html=True)
