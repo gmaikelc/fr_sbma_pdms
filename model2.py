d2d76389bc5a64c4fe012f866a7da8086f6c6f58
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-12-16 23:36:01 -0600

    Add files via upload

---
+++ model2.py
@@ -0,0 +1,356 @@
+
+
+
+def reading_reorder2(data, loaded_desc):
+        
+    #Select the specified columns from the DataFrame
+    df_selected = data[loaded_desc]
+    df_id = data.reset_index()
+    df_id.rename(columns={'MolID': 'NAME'}, inplace=True)
+    id = df_id['NAME'] 
+    # Order the DataFrame by the specified list of columns
+    test_data = df_selected.reindex(columns=loaded_desc)
+    # Fill missing values with 0
+    #test_data = test_data.fillna(0)
+    #descriptors_total = data[loaded_desc]
+
+    return test_data, id
+
+
+def mixture_descriptors2(data1, data2):
+    # Extract component fractions
+    #sbma_mw_unit = 280.41
+    #pdms_mw_unit = 92.12
+
+    #fraction_sbma = sbma_mw/sbma_mw_unit
+    #fraction_pdms = pdms_mw/pdms_mw_unit
+    
+    component1 = fraction_sbma  #data['Component1']
+    component2 = fraction_pdms #data['Component2']
+
+    # Multiply corresponding rows in data1 and data2 for all columns
+    df_mixture_left = component1* test_data1
+    df_mixture_right = component2* test_data2
+
+    #st.dataframe(df_mixture_left)
+    #st.dataframe(df_mixture_right)
+
+    df_mixture_left = df_mixture_left.reset_index(drop=True)
+    df_mixture_right = df_mixture_right.reset_index(drop=True)
+    
+    # Sum the DataFrames row-wise by column name
+    df_sum_mixture_ini = df_mixture_left.add(df_mixture_right)
+    # Remove the column index from the dataframe 
+    df_sum_mixture_ini = df_sum_mixture_ini.iloc[:,0:]
+    #st.write('dataframe mixture descriptors')
+    #st.dataframe(df_sum_mixture_ini)
+    #st.write(choice)
+    # Multiply the DataFrame by the selected percentage
+    df_sum_mixture = df_sum_mixture_ini * percentage
+    #st.write('dataframe  by percent added')
+    #st.dataframe(df_sum_mixture)
+
+    return df_sum_mixture
+
+
+def normalize_data2(train_data, test_data):
+    # Normalize the training data
+    df_train = pd.DataFrame(train_data)
+    saved_cols = df_train.columns
+    min_max_scaler = preprocessing.MinMaxScaler().fit(df_train)
+    np_train_scaled = min_max_scaler.transform(df_train)
+    df_train_normalized = pd.DataFrame(np_train_scaled, columns=saved_cols)
+
+    # Normalize the test data using the scaler fitted on training data
+    np_test_scaled = min_max_scaler.transform(test_data)
+    df_test_normalized = pd.DataFrame(np_test_scaled, columns=saved_cols)
+
+    #st.write('Normalized data')
+    #st.dataframe(df_test_normalized)
+
+    #st.write('Train normalized')
+    #st.dataframe(df_train_normalized.head(5))
+                 
+    return df_train_normalized, df_test_normalized
+
+
+def applicability_domain2(df_test_normalized, df_train_normalized):
+    y_train=data_train_2['c_lytica_removal_at_20psi']
+    X_train = df_train_normalized.values
+    X_test = df_test_normalized.values
+
+    # Add a small value to the diagonal
+    epsilon = 1e-10
+    XTX = X_train.T @ X_train
+    XTX += np.eye(XTX.shape[0]) * epsilon
+
+    # Compute the hat matrix
+    hat_matrix_train = X_train @ np.linalg.inv(XTX) @ X_train.T
+
+    # Calculate leverage and standard deviation for the training set
+    #hat_matrix_train = X_train @ np.linalg.inv(X_train.T @ X_train) @ X_train.T
+    
+    leverage_train = np.diagonal(hat_matrix_train)
+    leverage_train=leverage_train.ravel()
+    
+    # Calculate leverage and standard deviation for the test set
+    hat_matrix_test = X_test @ np.linalg.inv(XTX) @ X_test.T
+    #hat_matrix_test = X_test @ np.linalg.inv(X_train.T @ X_train) @ X_test.T
+    leverage_test = np.diagonal(hat_matrix_test)
+    leverage_test=leverage_test.ravel()
+
+
+    from sklearn.metrics import mean_squared_error
+
+    # Train a random forest model
+    from sklearn.ensemble import RandomForestRegressor
+
+    lr = RandomForestRegressor(
+        n_estimators=100,        # Number of trees in the forest
+        max_depth=10,           # Max depth of each tree
+        min_samples_split=2,    # Minimum samples required to split an internal node
+        random_state=42         # For reproducibility
+    )
+    
+    lr.fit(df_train_normalized, y_train)
+    y_pred_train = lr.predict(df_train_normalized)
+    
+    std_dev_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+    std_residual_train = (y_train - y_pred_train) / std_dev_train
+    std_residual_train = std_residual_train.ravel()
+    
+    # threshold for the applicability domain
+    
+    h3 = 3*((df_train_normalized.shape[1]+1)/df_train_normalized.shape[0])  
+    
+    diagonal_compare = list(leverage_test)
+    h_results =[]
+    for valor in diagonal_compare:
+        if valor < h3:
+            h_results.append(True)
+        else:
+            h_results.append(False)         
+    return h_results, leverage_train, leverage_test, std_residual_train 
+
+# Function to assign colors based on confidence values
+def get_color(confidence):
+    """
+    Assigns a color based on the confidence value.
+
+    Args:
+        confidence (float): The confidence value.
+
+    Returns:
+        str: The color in hexadecimal format (e.g., '#RRGGBB').
+    """
+    # Define your color logic here based on confidence
+    if confidence == "HIGH" or confidence == "Inside AD":
+        return 'green'
+    elif confidence == "MEDIUM":
+        return 'yellow'
+    else:
+        confidence ==  "LOW"
+        return 'red'
+
+
+
+#%% Predictions   
+
+def predictions2(loaded_model, loaded_desc, df_test_normalized):
+    scores = []
+    h_values = []
+    std_resd = []
+    idx = data['ID']
+    
+
+    #descriptors_model = loaded_desc
+    # Placeholder for the spinner
+    with st.spinner('CALCULATING PREDICTIONS (STEP 2 OF 3)...'):
+        # Simulate a long-running computation
+        time.sleep(1)  # Sleep for 5 seconds to mimic computation
+        
+        y_train=data_train_2['c_lytica_removal_at_20psi']
+        X = df_test_normalized
+        #X = df_test_normalized[descriptors_model]
+        loaded_model.fit(df_train_normalized,y_train)
+        predictions = loaded_model.predict(X)
+        scores.append(predictions)
+        
+        # y_true and y_pred are the actual and predicted values, respectively
+    
+        # Create y_true array with all elements set to mean value and the same length as y_pred
+        y_pred_test = predictions
+        y_test = np.full_like(y_pred_test, mean_value)
+        residuals_test = y_test -y_pred_test
+
+        std_dev_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+        std_residual_test = (y_test - y_pred_test) / std_dev_test
+        std_residual_test = std_residual_test.ravel()
+          
+        std_resd.append(std_residual_test)
+        
+        h_results, leverage_train, leverage_test, std_residual_train  = applicability_domain2(df_test_normalized, df_train_normalized)
+        h_values.append(h_results)
+    
+
+        dataframe_pred = pd.DataFrame(scores).T
+        dataframe_pred.index = idx
+        dataframe_pred.rename(columns={0: "C. lytica percent removal at 10 psi"},inplace=True)
+    
+        dataframe_std = pd.DataFrame(std_resd).T
+        dataframe_std.index = idx
+          
+        
+        h_final = pd.DataFrame(h_values).T
+        h_final.index = idx
+        h_final.rename(columns={0: "Confidence"},inplace=True)
+
+        std_ensemble = dataframe_std.iloc[:,0]
+        # Create a mask using boolean indexing
+        std_ad_calc = (std_ensemble >= 3) | (std_ensemble <= -3) 
+        std_ad_calc = std_ad_calc.replace({True: 'Outside AD', False: 'Inside AD'})
+   
+    
+        final_file = pd.concat([std_ad_calc,h_final,dataframe_pred], axis=1)
+    
+        final_file.rename(columns={0: "Std_residual"},inplace=True)
+    
+        h3 = 3*((df_train_normalized.shape[1]+1)/df_train_normalized.shape[0])  ##  Mas flexible
+
+        final_file.loc[(final_file["Confidence"] == True) & ((final_file["Std_residual"] == 'Inside AD' )), 'Confidence'] = 'HIGH'
+        final_file.loc[(final_file["Confidence"] == True) & ((final_file["Std_residual"] == 'Outside AD')), 'Confidence'] = 'LOW'
+        final_file.loc[(final_file["Confidence"] == False) & ((final_file["Std_residual"] == 'Outside AD')), 'Confidence'] = 'LOW'
+        final_file.loc[(final_file["Confidence"] == False) & ((final_file["Std_residual"] == 'Inside AD')), 'Confidence'] = 'MEDIUM'
+
+
+            
+        df_no_duplicates = final_file[~final_file.index.duplicated(keep='first')]
+        styled_df = df_no_duplicates.style.apply(lambda row: [f"background-color: {get_color(row['Confidence'])}" for _ in row],subset=["Confidence"], axis=1)
+    
+        return final_file, styled_df,leverage_train,std_residual_train, leverage_test, std_residual_test
+
+#Calculating the William's plot limits
+def calculate_wp_plot_limits2(leverage_train,std_residual_train, x_std_max=4, x_std_min=-4):
+    
+    with st.spinner('CALCULATING APPLICABILITY DOMAIN (STEP 3 OF 3)...'):
+        # Simulate a long-running computation
+        time.sleep(1)  # Sleep for 5 seconds to mimic computation
+        # Getting maximum std value
+        if std_residual_train.max() < 4:
+            x_lim_max_std = x_std_max
+        elif std_residual_train.max() > 4:
+            x_lim_max_std = round(std_residual_train.max()) + 1
+
+        # Getting minimum std value
+        if std_residual_train.min() > -4:
+            x_lim_min_std = x_std_min
+        elif std_residual_train.min() < 4:
+            x_lim_min_std = round(std_residual_train.min()) - 1
+
+    
+        #st.write('x_lim_max_std:', x_lim_max_std)
+        #st.write('x_lim_min_std:', x_lim_min_std)
+
+        # Calculation H critical
+        n = len(leverage_train)
+        p = df_train_normalized.shape[1]
+        h_value = 3 * (p + 1) / n
+        h_critical = round(h_value, 4)
+        #st.write('Number of cases training:', n)
+        #st.write('Number of variables:', p)
+        #st.write('h_critical:', h_critical)
+
+        # Getting maximum leverage value
+        if leverage_train.max() < h_critical:
+            x_lim_max_lev = h_critical + h_critical * 0.5
+        elif leverage_train.max() > h_critical:
+            x_lim_max_lev = leverage_train.max() + (leverage_train.max()) * 0.1
+
+        # Getting minimum leverage value
+        if leverage_train.min() < 0:
+            x_lim_min_lev = x_lev_min - x_lev_min * 0.05
+        elif leverage_train.min() > 0:
+            x_lim_min_lev = 0
+
+        #st.write('x_lim_max_lev:', x_lim_max_lev)
+
+        return x_lim_max_std, x_lim_min_std, h_critical, x_lim_max_lev, x_lim_min_lev
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+def williams_plot2(leverage_train, leverage_test, std_residual_train, std_residual_test,id_list_1,
+                  plot_color='cornflowerblue', show_plot=True, save_plot=False, filename=None, add_title=False, title=None):
+    fig = go.Figure()
+
+    # Add training data points
+    fig.add_trace(go.Scatter(
+        x=leverage_train,
+        y=std_residual_train,
+        mode='markers',
+        marker=dict(color='cornflowerblue', size=10, line=dict(width=1, color='black')),
+        name='Training'
+    ))
+
+    # Add test data points
+    fig.add_trace(go.Scatter(
+        x=leverage_test,
+        y=std_residual_test,
+        mode='markers',
+        marker=dict(color='orange', size=10, line=dict(width=1, color='black')),
+        name='Prediction',
+        text = id_list_1, # Add compounds IDs for hover
+        hoverinfo = 'text' #Show only the text when hovering
+    ))
+
+    # Add horizontal and vertical dashed lines
+    fig.add_shape(type='line', x0=h_critical, y0=x_lim_min_std, x1=h_critical, y1=x_lim_max_std,
+                  line=dict(color='black', dash='dash'))
+    fig.add_shape(type='line', x0=x_lim_min_lev, y0=3, x1=x_lim_max_lev, y1=3,
+                  line=dict(color='black', dash='dash'))
+    fig.add_shape(type='line', x0=x_lim_min_lev, y0=-3, x1=x_lim_max_lev, y1=-3,
+                  line=dict(color='black', dash='dash'))
+
+    # Add rectangles for outlier zones
+    fig.add_shape(type='rect', x0=x_lim_min_lev, y0=x_lim_min_std, x1=h_critical, y1=-3,
+                  fillcolor='lightgray', opacity=0.4, line_width=0)
+    fig.add_shape(type='rect', x0=x_lim_min_lev, y0=3, x1=h_critical, y1=x_lim_max_std,
+                  fillcolor='lightgray', opacity=0.4, line_width=0)
+                      
+    fig.add_shape(type='rect', x0=h_critical, y0=x_lim_min_std, x1=x_lim_max_lev, y1=-3,
+                  fillcolor='lightgray', opacity=0.4, line_width=0)
+    fig.add_shape(type='rect', x0=h_critical, y0=3, x1=x_lim_max_lev, y1=x_lim_max_std,
+                  fillcolor='lightgray', opacity=0.4, line_width=0)
+
+    # Add annotations for outlier zones
+    fig.add_annotation(x=(h_critical + x_lim_min_lev) / 2, y=-3.5, text='Outlier zone', showarrow=False,
+                       font=dict(size=15))
+    fig.add_annotation(x=(h_critical + x_lim_min_lev) / 2, y=3.5, text='Outlier zone', showarrow=False,
+                       font=dict(size=15))
+    fig.add_annotation(x=(h_critical + x_lim_max_lev) / 2, y=-3.5, text='Outlier zone', showarrow=False,
+                       font=dict(size=15))
+    fig.add_annotation(x=(h_critical + x_lim_max_lev) / 2, y=3.5, text='Outlier zone', showarrow=False,
+                       font=dict(size=15))
+
+    # Update layout
+    fig.update_layout(
+        width=600,
+        height=600,
+        xaxis=dict(title='Leverage', range=[x_lim_min_lev, x_lim_max_lev], tickfont=dict(size=15)),
+        yaxis=dict(title='Std Residuals', range=[x_lim_min_std, x_lim_max_std], tickfont=dict(size=15)),
+        legend=dict(x=0.99, y=0.825, xanchor='right', yanchor='top', font=dict(size=20)),
+        showlegend=True
+    )
+
+    if add_title and title:
+        fig.update_layout(title=dict(text=title, font=dict(size=20)))
+
+    if save_plot and filename:
+        fig.write_image(filename)
+
+    if show_plot:
+        fig.show()
+
+    return fig
+	
