a64c1e7769b7319bedd587423a89adde760dd5a3
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-12-17 12:58:31 -0600

    Update model2.py

--- model2.py
+++ model2.py
@@ -29,7 +29,6 @@ def mixture_descriptors2(data21, data22,fraction_sbma,fraction_pdms):
      
 
     # Multiply corresponding rows in data1 and data2 for all columns
-    
     #df_mixture_left2 = fraction_sbma* test_data21
     #df_mixture_right2 = fraction_pdms* test_data22
 
